e8bb85f15c1017da6081b93b64a60eeaabe13a49
RayoKh <112399386+RayoKh@users.noreply.github.com>
2023-07-27 16:38:03 +0000

    Deploying to gh-pages from @ RayoKh/goit-js-hw-09@637e11b1fee68a1ae8320c43ce041900847e1f51 🚀

---
+++ 01-color-switcher.1e5aa21a.js
@@ -0,0 +1,2 @@
+const t={body:document.body,startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};t.stopBtn.disabled=!0;let n=null;t.startBtn.addEventListener("click",(function(){if(t.startBtn.disabled)return;t.startBtn.disabled=!0,t.stopBtn.disabled=!1,n=setInterval((()=>{const n=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.body.style.backgroundColor=`${n}`}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.1e5aa21a.js.map
